a2e3328ab719f12d3b946955afe01af84687f79b
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-09 16:43:26 +0530

    added migrations

--- tweets/models.py
+++ tweets/models.py
@@ -1,13 +1,29 @@
+
+# import settings for auth
+
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 # bring all the models into the admin 
 # so that admin will see it
+# every time when make changes to the models do manage.py makemigrations
 
-class Tweet(models.Model):
+class Tweet(models.Model):    
+    # a user to which this tweet is associated
+    user = models.ForeignKey(settings.AUTH_USER_MODEL) 
+    tweet_content = models.CharField(max_length=140,default="post your tweet here")
+    
 
-    this_is_a_tweet = models.TextField(default="content1")
+    #tweet_number = models.IntegerField(default=1)
 
-    def __str__(self):
-        return str(self.this_is_a_tweet)
+
+
+
+
+
+
+
+
